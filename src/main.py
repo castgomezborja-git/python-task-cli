f37278c87c302fafd55c82bcc8d9e7b0e921def1
Borja Castano <castgomezborja@gmail.com>
2026-04-07 17:01:11 +0200

    feat: add task creation and listing functionality

--- src/main.py
+++ src/main.py
@@ -1,10 +1,25 @@
-from tasks import load_tasks, save_tasks
+from tasks import load_tasks, save_tasks, add_task, list_tasks
 
 def main():
-    print("📝 Gestor de tareas iniciado")
-
     tasks = load_tasks()
-    print(f"Tienes {len(tasks)} tareas")
+
+    while True:
+        print("\n1. Ver tareas")
+        print("2. Añadir tarea")
+        print("3. Salir")
+
+        option = input("Elige opción: ")
+
+        if option == "1":
+            list_tasks(tasks)
+
+        elif option == "2":
+            title = input("Título: ")
+            tasks = add_task(tasks, title)
+            save_tasks(tasks)
+
+        elif option == "3":
+            break
 
 if __name__ == "__main__":
     main()
--- src/tasks.py
+++ src/tasks.py
@@ -18,3 +18,17 @@ def load_tasks():
 def save_tasks(tasks):
     with open(FILE_PATH, "w") as file:
         json.dump(tasks, file, indent=4)
+
+def add_task(tasks, title):
+    task = {
+        "id": len(tasks) + 1,
+        "title": title,
+        "completed": False
+    }
+    tasks.append(task)
+    return tasks
+
+def list_tasks(tasks):
+    for task in tasks:
+        status = "✅" if task["completed"] else "❌"
+        print(f'{task["id"]}. {task["title"]} [{status}]')
